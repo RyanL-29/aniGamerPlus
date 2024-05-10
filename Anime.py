5757658042bb500a5f7f6e1533e382101e594ec1
Ryan <facebook0209@gmail.com>
2024-05-10 22:08:42 +0800

    temp remove check

--- Anime.py
+++ Anime.py
@@ -693,8 +693,8 @@ class Anime:
                                            max_retry=self._settings['segment_max_retry']).content)
                     chunk_size = os.path.getsize(chunk_local_path)
                     err_print(self._sn, '下載狀態', 'Segment Downloaded=' + chunk_name + ' Size=' + str(chunk_size), status=0, display=False)
-                    if chunk_size < 1 and index != len(chunk_list) - 1:
-                        raise ChunkSizeInvalid('任務狀態: sn=' + str(self._sn) + ' 請求所獲取的檔案不完整！請求鏈接：\n%s' % uri)
+                    # if chunk_size < 1 and index != len(chunk_list) - 1:
+                    #     raise ChunkSizeInvalid('任務狀態: sn=' + str(self._sn) + ' 請求所獲取的檔案不完整！請求鏈接：\n%s' % uri)
             except TryTooManyTimeError:
                 failed_flag = True
                 err_print(self._sn, '下載狀態', 'Bad segment=' + chunk_name, status=1)

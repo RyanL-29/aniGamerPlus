cf7cddf902c8345345b7896a1f0f1569964021c9
Ryan <facebook0209@gmail.com>
2024-06-29 03:16:58 +0800

    akamiz file last chunk may get 0

--- Anime.py
+++ Anime.py
@@ -695,7 +695,7 @@ class Anime:
                     f.write(req.content)
                     chunk_size = os.path.getsize(chunk_local_path)
                     err_print(self._sn, '下載狀態', 'Segment Downloaded=' + chunk_name + ' Size=' + str(chunk_size) + ' ExpectedSize=' + str(req.headers.get('Content-Length')), status=0, display=False)
-                    if int(chunk_size) != int(req.headers.get('Content-Length')):
+                    if int(chunk_size) != int(req.headers.get('Content-Length')) and index != (len(chunk_list) - 1):
                         err_print(self._sn, '下載狀態', '任務狀態: sn=' + str(self._sn) + ' 請求所獲取的檔案不完整！請求鏈接：\n%s' % uri, status=0, display=False)
                         raise ChunkSizeInvalid('任務狀態: sn=' + str(self._sn) + ' 請求所獲取的檔案不完整！請求鏈接：\n%s' % uri)
             except TryTooManyTimeError:
